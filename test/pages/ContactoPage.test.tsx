5d4e7278f76b4767d60547804227a6e147ec4d50
Maicol Antonio Saldivia Silva <mai.saldivia@duocuc.cl>
2025-11-15 18:47:38 -0300

    docs: actualizar README con descripción del proyecto y lista de 10 tests; fix ContactoPage test import

--- test/pages/ContactoPage.test.tsx
+++ test/pages/ContactoPage.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { act } from "react";
 import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 
 import { ContactoPage } from "../../src/pages/ContactoPage";
